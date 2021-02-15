c6c1721141d2cf99396615cec15ee6e12313aa77
yamamotoamon <73935940+yamamotoamon@users.noreply.github.com>
2021-02-15 23:20:50 +0900

    男女別の栄養計算

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'nutritions#index'
   resources :nutritions, only: [:index, :new]
+  get 'nutritions/index2', to: 'nutritions#index2'
+  get 'nutritions/new2', to: 'nutritions#new2'
 end
